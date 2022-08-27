3923412a2ab1b255f9efef83c78d54da0f5198a8
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-08-27 13:17:39 -0500

    first commit

---
+++ psudeocode.js
@@ -0,0 +1,32 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for my team members and their information
+// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+
+
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+
+
+// WHEN I click on the GitHub username
+// THEN that GitHub profile opens in a new tab
+    // opening a new tab HTML way
+        // <a href='url' target='_blank' rel = 'noopener'>GitHub username</a>
+
+// WHEN I start the application
+// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+    // set up an employee class
+
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+    // extend employee class
+
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+    // extend employee class
+
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+    // extend employee class
+
+// WHEN I decide to finish building my team
+// THEN I exit the application, and the HTML is generated
